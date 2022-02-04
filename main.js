1e57e93edcaf31d88e163aa821e26f9e7cc6964e
nbs252 <87271185+nbs252@users.noreply.github.com>
2022-02-04 12:11:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+song_1 = "";
+song_2 = "";
+
+function setup(){
+canvas = createCanvas(600, 500);
+canvas.center()
+
+video = 
+}
+
+function preload(){
+song_1 = loadSound("music.mp3");
+song_2 = loadSound("music2.mp3");
+}
